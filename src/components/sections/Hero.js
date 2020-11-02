2583cba126aa3908a14f8a0333e2fc757dda3678
Vinnie <viniciussilva@ciandt.com>
2020-11-01 21:13:35 -0300

    [S2] Pointed links to Personal4All

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -75,10 +75,10 @@ const Hero = ({
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
+                  <Button tag="a" color="primary" wideMobile href="#featuresTiles">
                     Saiba Mais
                     </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
+                  <Button tag="a" color="dark" wideMobile href="http://www.personal4all.com.br/">
                     Entrar
                     </Button>
                 </ButtonGroup>
